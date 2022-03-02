8afb39463c0cad7c69e498a08035b09a7b68368f
alejandro Lopez <alejandrolpz0619@gmail.com>
2022-03-02 18:31:16 -0400

    add: send_notification function triggers to send desktop notifications. feat: now it sends a notification once the video is downloaded.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,2 +1,3 @@
 pub mod fetch;
 pub mod get_directory;
+pub mod notification;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod backend;
 use backend::{fetch::api, get_directory};
 use clap::Parser;
+//use notify_rust::Notification;
 // CLI
 #[derive(Debug, Parser)]
 #[clap(
@@ -27,10 +28,9 @@ async fn main() {
     let (keys, links, _available_q, video_id, video_title) =
         api::fetch_video_info(&args.link).await.unwrap();
     // Remove / if there's any
-    let video_title = video_title.replace("/", "");
+    let video_title = video_title.replace('/', "");
     // Get the video key depeding on the quality entered by the user
-    let video_key: String = api::get_links_by_quality("720p", (keys, links));
-    println!("{}", video_title);
+    let video_key: String = api::get_links_by_quality(&args.quality, (keys, links));
     let download_link = api::get_download_link(video_id, video_key).await.unwrap();
     api::download_video(
         download_link,
